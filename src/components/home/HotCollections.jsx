fb1977f2652e569c4344e2a3d3a9232051300e9f
z h <zhanif02@gmail.com>
2024-11-15 14:29:06 +0000

    skeleton loading state added to hot collections

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -4,22 +4,25 @@ import { Link } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import '../../css/styles/style.css'
+import '../../css/styles/style.css';
+import Skeleton from '../UI/Skeleton';
+
 const HotCollections = () => {
   const [collections, setCollections] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); 
 
-  
   useEffect(() => {
     const fetchCollections = async () => {
+      setIsLoading(true); 
       const { data } = await axios.get(
         `https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`
       );
       setCollections(data);
+      setIsLoading(false); 
     };
+
     fetchCollections();
-  }, []); 
-
-
+  }, []);
 
   function PrevArrow(props) {
     const { className, style, onClick } = props;
@@ -33,7 +36,7 @@ const HotCollections = () => {
       </button>
     );
   }
-  
+
   function NextArrow(props) {
     const { className, style, onClick } = props;
     return (
@@ -47,43 +50,79 @@ const HotCollections = () => {
     );
   }
 
-  
   const settings = {
     infinite: true,
-    slidesToShow: 4,    
+    slidesToShow: 4,
     slidesToScroll: 1,
     arrows: true,
     dots: false,
     responsive: [
       {
-        breakpoint: 1200, 
+        breakpoint: 1200,
         settings: {
           slidesToShow: 4,
         },
       },
       {
-        breakpoint: 992,  
+        breakpoint: 992,
         settings: {
           slidesToShow: 3,
         },
       },
       {
-        breakpoint: 768,  
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
         },
       },
       {
-        breakpoint: 480,  
+        breakpoint: 480,
         settings: {
           slidesToShow: 1,
         },
       },
-    ],  prevArrow: <PrevArrow />,
+    ],
+    prevArrow: <PrevArrow />,
     nextArrow: <NextArrow />,
   };
 
-  return (
+  return isLoading ? (
+    <section id="section-collections" className="no-bottom">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="text-center">
+              <h2>Hot Collections</h2>
+              <div className="small-border bg-color-2"></div>
+            </div>
+          </div>
+          {new Array(4).fill(0).map((_, index) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+              <div className="nft_coll">
+                <div className="nft_wrap">
+                  <Skeleton width="100%" height="190px" borderRadius="5px" />
+                </div>
+                <div className="nft_coll_pp">
+                  <Skeleton width="40px" height="40px" borderRadius="50%" />
+                  <i className="fa fa-check"></i>
+                </div>
+                <div className="nft_coll_info">
+                  <Link to="/explore">
+                    <Skeleton
+                      width="160px"
+                      height="25px"
+                      borderRadius="5px"
+                    />
+                  </Link>
+                </div>
+                <Skeleton width="120px" height="25px" borderRadius="5px" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  ) : (
     <section id="section-collections" className="no-bottom">
       <div className="container">
         <div className="row">
